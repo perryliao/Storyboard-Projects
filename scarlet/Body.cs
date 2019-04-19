e28704183532383bf86ff4f8770b4cc865b0a3d4
Perry Liao <perryliaohc@gmail.com>
2019-04-19 14:37:23 -0700

    put body parts in a loop, update positions

--- scarlet/Body.cs
+++ scarlet/Body.cs
@@ -22,10 +22,11 @@ namespace StorybrewScripts
             var bodyHighlight = layer.CreateSprite("sb/body/body_highlight.png", OsbOrigin.Centre);
             var bodyPixelate = layer.CreateSprite("sb/body/body_pixelate.png", OsbOrigin.Centre);
 
-            bodyMain.Scale(0, 0.40);
-            bodyDarken.Scale(0, 0.40);
-            bodyHighlight.Scale(0, 0.40);
-            bodyPixelate.Scale(0, 0.40);
+            OsbSprite[] allParts = new OsbSprite[] { bodyMain, bodyDarken, bodyHighlight, bodyPixelate };
+            foreach (OsbSprite elem in allParts) {
+                elem.Scale(0, 0.45);
+                elem.Move(0, 50, 240);
+            }
 
             bodyMain.Fade(OsbEasing.In, 5623, 5801, 0, 1);
             bodyDarken.Fade(OsbEasing.In, 62428, 62606, 0, 1);
