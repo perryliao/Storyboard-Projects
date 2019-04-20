669e279162763192e6b6afa6ef416840d55ed338
Perry Liao <perryliaohc@gmail.com>
2019-04-19 21:28:35 -0700

    removed all hard coded values

--- scarlet/Gears.cs
+++ scarlet/Gears.cs
@@ -24,44 +24,25 @@ namespace StorybrewScripts
         public bool clockwise = true;
         [Configurable]
         public double scale = 1.0;
+        [Configurable]
+        public double xValue = 0.0;
+        [Configurable]
+        public double yValue = 0.0;
+
+        [Configurable]
+        public double rotationAmount = 1/8;
         
         public double beatLength = 356;
         public override void Generate()
         {
 		    var layer = GetLayer("gears");
-            OsbSprite[] gears = new OsbSprite[] {
-                    layer.CreateSprite("sb/gears/gear0.png", OsbOrigin.Centre),
-                    layer.CreateSprite("sb/gears/gear1.png", OsbOrigin.Centre),
-                    layer.CreateSprite("sb/gears/gear2.png", OsbOrigin.Centre),
-                    layer.CreateSprite("sb/gears/gear3.png", OsbOrigin.Centre),
-                    layer.CreateSprite("sb/gears/gear4.png", OsbOrigin.Centre),
-                    layer.CreateSprite("sb/gears/gear5.png", OsbOrigin.Centre)
-                 };
-            
-            int count = 0;
-            foreach(OsbSprite gear in gears) {
-                gear.Move(0, 0, 0);
-                gear.Scale(0, 0.3); // TODO remove
-                gear.Fade(OsbEasing.InExpo, startTime - beatLength/2, startTime, 0, 1);
-                gear.Fade(OsbEasing.OutExpo, endTime - beatLength/2, endTime, 1, 0);
+            OsbSprite gear = layer.CreateSprite("sb/gears/gear" + gearNumber + ".png", OsbOrigin.Centre);
 
-                gear.Rotate(startTime - beatLength/2, endTime, 0, Math.PI/8 * (clockwise ? -1 : 1));
-                count++;
-            }
-            
-            // First section 
-            gears[0].Scale(0, 0.9);
-            gears[0].Move(0, 620, 410);
-
-            gears[1].Scale(0, 0.6);
-            gears[1].Move(0, 320, 410);
-            gears[1].Rotate(0, Math.PI*3/Math.Pow(2, 8));
-            
-            gears[3].Scale(0, 0.45);
-            gears[2].Move(0, 200, 200);
-
-            gears[3].Scale(0, 0.45);
-            gears[3].Move(0, 620, 100);
+            gear.Scale(0, scale);
+            gear.Move(startTime - beatLength/2, xValue, yValue);
+            gear.Fade(OsbEasing.InExpo, startTime - beatLength/2, startTime, 0, 1);
+            gear.Fade(OsbEasing.OutExpo, endTime - beatLength/2, endTime, 1, 0);
+            gear.Rotate(startTime - beatLength/2, endTime, 0, Math.PI*rotationAmount * (clockwise ? -1 : 1));
         }
     }
 }
