e8d22da8bba9ac8966b7af562e76d9027b989758
Perry Liao <perryliaohc@gmail.com>
2019-04-19 21:14:52 -0700

    added configurable variables

--- scarlet/Gears.cs
+++ scarlet/Gears.cs
@@ -14,9 +14,18 @@ namespace StorybrewScripts
 {
     public class Gears : StoryboardObjectGenerator
     {
+        [Configurable]
+        public string gearNumber = "0";
+        [Configurable]
         public double startTime = 1363;
-        public double endTime1 = 2073;
-
+        [Configurable]
+        public double endTime = 2073;
+        [Configurable]
+        public bool clockwise = true;
+        [Configurable]
+        public double scale = 1.0;
+        
+        public double beatLength = 356;
         public override void Generate()
         {
 		    var layer = GetLayer("gears");
@@ -33,10 +42,10 @@ namespace StorybrewScripts
             foreach(OsbSprite gear in gears) {
                 gear.Move(0, 0, 0);
                 gear.Scale(0, 0.3); // TODO remove
-                gear.Fade(OsbEasing.InExpo, 1186, startTime, 0, 1);
-                gear.Fade(endTime1, 0);
+                gear.Fade(OsbEasing.InExpo, startTime - beatLength/2, startTime, 0, 1);
+                gear.Fade(OsbEasing.OutExpo, endTime - beatLength/2, endTime, 1, 0);
 
-                gear.Rotate(1186, endTime1, 0, Math.PI/8 * ((count % 2) == 1 ? -1 : 1));
+                gear.Rotate(startTime - beatLength/2, endTime, 0, Math.PI/8 * (clockwise ? -1 : 1));
                 count++;
             }
             
@@ -47,7 +56,8 @@ namespace StorybrewScripts
             gears[1].Scale(0, 0.6);
             gears[1].Move(0, 320, 410);
             gears[1].Rotate(0, Math.PI*3/Math.Pow(2, 8));
-
+            
+            gears[3].Scale(0, 0.45);
             gears[2].Move(0, 200, 200);
 
             gears[3].Scale(0, 0.45);
