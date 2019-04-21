1df08fdf4f38ad7d1dd87f41a52fd9786f4bd590
Perry Liao <perryliaohc@gmail.com>
2019-04-20 17:37:38 -0700

    Created a paint class

---
+++ scarlet/Paint.cs
@@ -0,0 +1,60 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Mapset;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Storyboarding.Util;
+using StorybrewCommon.Subtitles;
+using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class Paint : StoryboardObjectGenerator
+    {
+        [Configurable]
+        public double startTime = 5801;
+        [Configurable]
+        public int numIterations = 4;
+
+        private OsbSprite findValidEntry( bool[] arr ) {
+		        var layer = GetLayer("paint");
+                Random rnd = new Random();
+                int idx = rnd.Next(arr.Length);
+                while(!arr[idx]) {
+                    // if arr[idx] == false, look for another index
+                    idx = rnd.Next(arr.Length);
+                }
+                // set arr[idx] to false and return the sprite found
+                arr[idx] = false;
+                return layer.CreateSprite("sb/paint/paint" + idx.ToString() + ".png", OsbOrigin.Centre);
+            }
+
+        private double measureLength = 7221 - 5801; 
+
+        public override void Generate()
+        {
+            bool[] boolArr = new bool[8];
+            for (int i = 0; i < boolArr.Length; i++) {
+                boolArr[i] = true; // initialize all to true
+            }
+
+            OsbSprite sprite = findValidEntry(boolArr);
+            int height = 140;
+            sprite.Fade(OsbEasing.InExpo, startTime - measureLength/16, startTime, 0, 1);
+            sprite.Move(OsbEasing.InExpo, startTime - measureLength/16, startTime, 150, height, 120, height);
+            sprite.Move(startTime, startTime + 2*measureLength - measureLength/4, sprite.PositionAt(startTime), 100, height);
+            sprite.Move(
+                OsbEasing.InExpo, 
+                startTime + 2*measureLength - measureLength/4, 
+                startTime + 2*measureLength - measureLength/8,
+                sprite.PositionAt(startTime + 2*measureLength - measureLength/4),
+                70,
+                height
+                );
+            sprite.Fade(OsbEasing.InExpo, startTime + 2*measureLength - measureLength/4, startTime + 2*measureLength - measureLength/8, 1, 0);
+        }
+    }
+}
