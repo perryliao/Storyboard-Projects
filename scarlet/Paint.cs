6c40f7f0164069bc70e5b2457db9e24af3546f06
Perry Liao <perryliaohc@gmail.com>
2019-04-20 18:02:44 -0700

    finalizing paint effect

--- scarlet/Paint.cs
+++ scarlet/Paint.cs
@@ -44,17 +44,17 @@ namespace StorybrewScripts
             for (int i = 0; i < numIterations; i++) {
                 double newStartTime = startTime + i * measureLength * 2;
                 OsbSprite sprite = findValidEntry(boolArr);
-                int height = 140;
+                int height = 160;
 
                 sprite.Fade(OsbEasing.InExpo, newStartTime - measureLength/16, newStartTime, 0, 1);
-                sprite.Move(OsbEasing.InExpo, newStartTime - measureLength/16, newStartTime, 150, height, 120, height);
+                sprite.Move(OsbEasing.InExpo, newStartTime - measureLength/16, newStartTime, 150, height, 110, height);
                 sprite.Move(newStartTime, newStartTime + 2*measureLength - measureLength/4, sprite.PositionAt(newStartTime), 100, height);
                 sprite.Move(
                     OsbEasing.InExpo, 
                     newStartTime + 2*measureLength - measureLength/4, 
                     newStartTime + 2*measureLength - measureLength/8,
                     sprite.PositionAt(newStartTime + 2*measureLength - measureLength/4),
-                    70,
+                    60,
                     height
                     );
                 sprite.Fade(OsbEasing.InExpo, newStartTime + 2*measureLength - measureLength/4, newStartTime + 2*measureLength - measureLength/8, 1, 0);
