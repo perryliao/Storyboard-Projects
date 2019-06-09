a7376d5b3e468bdae691f72f845b442d6cd168e4
Perry Liao <perryliaohc@gmail.com>
2019-06-09 01:10:32 -0700

    added title placements

---
+++ Heart/Title.cs
@@ -0,0 +1,34 @@
+using OpenTK;
+using OpenTK.Graphics;
+using StorybrewCommon.Mapset;
+using StorybrewCommon.Scripting;
+using StorybrewCommon.Storyboarding;
+using StorybrewCommon.Storyboarding.Util;
+using StorybrewCommon.Subtitles;
+using StorybrewCommon.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StorybrewScripts
+{
+    public class Title : StoryboardObjectGenerator
+    {
+        private double startTime = 23233;
+        private double endTime = 34527;
+        public override void Generate()
+        {
+		    var layer = GetLayer("Title");
+            OsbSprite[] words = new OsbSprite[2] {
+                layer.CreateSprite("sb/grim.png", OsbOrigin.CentreRight, new Vector2(310, 240)), 
+                layer.CreateSprite("sb/heart.png", OsbOrigin.CentreLeft, new Vector2(330, 240))
+                };
+
+            foreach (OsbSprite word in words) {
+                word.Fade(startTime, 1);
+                word.Scale(startTime, 0.5);
+                word.Fade(endTime, 0);
+            }
+        }
+    }
+}
