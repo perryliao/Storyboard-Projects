72f424f721f55518dd0fa61d74a668e92199fc5b
Perry Liao <perryliaohc@gmail.com>
2019-06-12 23:33:20 -0700

    use a for loop instead of loopgroup for thumping

--- Heart/Kokoro.cs
+++ Heart/Kokoro.cs
@@ -16,16 +16,36 @@ namespace StorybrewScripts
     {
         [Configurable]
         public double startTime = 292;
+
+        [Configurable]
+        public double endTime = 20409;
+
+        [Configurable]
+        public double slowFadeTime = 11939;
+
+        private double beatLength = 706;
+
         public override void Generate()
         {
 		    var layer = GetLayer("");
             OsbSprite kokoro = layer.CreateSprite("sb/Pool 1/heart.png", OsbOrigin.Centre);
             kokoro.Scale(startTime, 0.2);
 
-            kokoro.StartLoopGroup(startTime, 1);
-            kokoro.Fade(OsbEasing.OutBounce, startTime, 468, 0, 1);
-            // kokoro.Fade(OsbEasing.InExpo, 468, 644, 1, 0);
-            kokoro.EndGroup();
+            double timestep = beatLength * 2;
+            double i;
+            
+            if (slowFadeTime >= endTime) slowFadeTime = endTime;
+
+            for (i = startTime; i < slowFadeTime; i += timestep) {
+                kokoro.Fade(OsbEasing.OutBounce, i, i + beatLength / 4, 0, 0.6);
+                kokoro.Fade(OsbEasing.OutCirc, i + (beatLength / 4), i + (3 * beatLength / 8), 0.6, 1);
+                kokoro.Fade(OsbEasing.InCirc, i + (3 * beatLength / 8), i + (beatLength / 2) , 1, 0);
+            }
+
+            // kokoro.StartLoopGroup(startTime, 1);
+            // kokoro.Fade(OsbEasing.OutBounce, startTime, 468, 0, 1);
+            // // kokoro.Fade(OsbEasing.InExpo, 468, 644, 1, 0);
+            // kokoro.EndGroup();
         }
     }
 }
