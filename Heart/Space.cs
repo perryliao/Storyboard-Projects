10fbb2413d8121f1bb96d63bca0a14615e9bad59
Perry Liao <perryliaohc@gmail.com>
2019-06-26 23:05:43 -0700

    Fixed algebraic errors

--- Heart/Space.cs
+++ Heart/Space.cs
@@ -87,13 +87,14 @@ namespace StorybrewScripts
         private Vector2 calculateEndPoint(float spriteX, float spriteY) {
             // TODO: calculate end point of sprite based on given coordinates
             Vector2 origin = new Vector2(320, 240);
-            int radius = 5;
-
+            int radius = 25;
+            
+            Log("sX: "+ spriteX.ToString() + " xY: " + spriteY.ToString());
             // Formula of circle: (x-h)^2 + (y-k)^2 = r^2
             float h = origin.X;
             float k = origin.Y;
             // Formula of line: y = m*x + b
-            float m = (h - spriteX) / (k - spriteY);
+            float m = (k - spriteY)/(h - spriteX);
             float b = k - m*h;
 
             Log("h: "+ h.ToString() + " k: " + k.ToString());
@@ -107,8 +108,8 @@ namespace StorybrewScripts
             // use quadratic equation here...
 
             double quadA = Math.Pow(m, 2) + 1;
-            double quadB = 2*h + 2*m*b - 2*m*k;
-            double quadC = Math.Pow((b-k), 2) - Math.Pow(radius, 2);
+            double quadB = 2*m*(b-k) - 2*h;
+            double quadC = Math.Pow(h, 2) + Math.Pow((b-k), 2) - Math.Pow(radius, 2);
             Log("a: "+ quadA.ToString() + " b: " + quadB.ToString() + " c: " + quadC.ToString());
 
             double x1 = (-1*quadB + Math.Sqrt(Math.Pow(quadB, 2) - 4 * quadA * quadC))/(2*quadA);
