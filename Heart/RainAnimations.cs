5f8d075b1ac99f0cd3e812e485d5286fa4b473dc
Perry Liao <perryliaohc@gmail.com>
2019-06-30 23:09:30 -0700

    WIP attempt at making acceleration with rotating around a point

--- Heart/RainAnimations.cs
+++ Heart/RainAnimations.cs
@@ -25,28 +25,60 @@ namespace StorybrewScripts
             circle.Fade(37350, 0);
 
             circle.Move(OsbEasing.OutQuad, 34527, 34880, circle.PositionAt(34527), 320, 100);
-            moveAroundPointWithAcceleration(circle, 34880, (36644 - 34880) / 2, origin, Math.PI/4);
-            moveAroundPointWithDeceleration(circle, (36644 - 34880) / 2, 36644, origin, Math.PI);
+            moveAroundPointWithAcceleration(circle, 34880, (36644 - 34880) / 2, origin, 3*Math.PI/4);
+            moveAroundPointWithDeceleration(circle, (36644 - 34880) / 2, 36644, origin, 3*Math.PI/4);
         }
 
         /// <summary>Move a sprite around a point while accelerating (Similar to Easing.In)</summary>
         /// <param name="sprite">The sprite to animate</param>
         /// <param name="startTime">The starting time of the animation</param>
         /// <param name="endTime">The ending time of the animation</param>
-        /// <param name="point">The point to revolve the sprite around</param>
+        /// <param name="p">The point to revolve the sprite around</param>
         /// <param name="angle">Angle in radians to rotate</param>
-        private void moveAroundPointWithAcceleration(OsbSprite sprite, double startTime, double endTime, Vector2 point, double angle) {
+        private void moveAroundPointWithAcceleration(OsbSprite sprite, double startTime, double endTime, Vector2 p, double angle) {
             // TODO implement me
             double duration = endTime - startTime; 
-            float radius = (origin - new Vector2(sprite.PositionAt(startTime).X, sprite.PositionAt(startTime).Y)).Length;
+            Vector2 sP = new Vector2(sprite.PositionAt(startTime).X, sprite.PositionAt(startTime).Y);
+            float r = (origin - sP).Length;
             // a = v/t = d/t^2
+
+            double l = 2*Math.Sin(angle/2) * r; 
+            // l = sqrt((x - sP.X)^2 + (y - sP.Y)^2 ) => l^2 = (x - sP.X)^2 + (y - sP.Y)^2 => x^2 - 2*x*sP.X + sP.X^2 + y^2 - 2*y*sP.Y + sP.Y^2 - l^2 = 0 
+            // r = sqrt((x - p.X)^2 + (y - p.Y)^2 )   => r^2 = (x - p.X)^2 + (y - p.Y)^2   => x^2 - 2*x*p.X + p.X^2 + y^2 - 2*y*p.Y + p.Y^2 - r^2 = 0 
+            // Subtract eqations together...
+            // 2*x*sP.X - 2*x*p.X + p.X^2 - sP.X^2 - 2*y*p.Y + 2*y*sP.Y + p.Y^2 + sP.Y^2 + l^2 - r^2 = 0
+            double C = Math.Pow(p.X, 2) - Math.Pow(sP.X, 2) + Math.Pow(p.Y, 2) + Math.Pow(sP.Y, 2) - Math.Pow(l, 2) + Math.Pow(r, 2); // constants
+            Log(C.ToString());
+            // 2*x*sP.X - 2*x*p.X + 2*y*sP.Y - 2*y*p.Y + C = 0
+            // x(2*sP.X - 2*p.X) + y(2*sP.Y - 2*p.Y) + C = 0
+            // y = (x(2*sP.X - 2*p.X) + C)/-(2*sP.Y - 2*p.Y)
+            float m = -(sP.X - p.X)/(sP.Y - p.Y);
+            Log((sP.X - p.X).ToString() +" " + (sP.Y - p.Y).ToString());
+            float b = (float) C/(2*sP.Y - 2*p.Y);
+            // Use this line equation to find intersects with the circle with radius = r
             // Formula for circle: (x-h)^2 + (y-k)^2 = r^2
+            Vector2 dest = getNearestIntersectPoint(m, b, p.X, p.Y, r);
+            Log(dest.ToString());
+        }
 
-            double len = 2*Math.Sin(angle/2) * radius;
-            Log("angle = " + ( angle/2).ToString());
-            Log("sin = " + Math.Sin(angle/2).ToString());
-            Log("len = " + len.ToString());
+        private Vector2 getNearestIntersectPoint(float m, float b, float h, float k, float r) {
+            Log(m.ToString() + " " + b.ToString() + " " + h.ToString() + " " + k.ToString() + " " + r.ToString()); 
+            double quadA = Math.Pow(m, 2) + 1;
+            double quadB = 2*m*(b-k) - 2*h;
+            double quadC = Math.Pow(h, 2) + Math.Pow((b-k), 2) - Math.Pow(r, 2);
+            float x1 = (float) ((-1*quadB + Math.Sqrt(Math.Pow(quadB, 2) - 4 * quadA * quadC))/(2*quadA));
+            float x2 = (float) ((-1*quadB - Math.Sqrt(Math.Pow(quadB, 2) - 4 * quadA * quadC))/(2*quadA));
 
+            // Plug x coordinates back to origin line equation
+            float y1 = (float) m*x1 + b;
+            float y2 = (float) m*x2 + b;
+
+            Vector2 o = new Vector2(h, k);
+            Vector2 p1 = new Vector2(x1, y1);
+            Vector2 p2 = new Vector2(x2, y2);
+
+            // Pick the point closest to the sprite
+            return (o-p1).Length < (o-p2).Length ? p1 : p2;
         }
 
         /// <summary>Move a sprite around a point while decelerating (Similar to Easing.Out)</summary>
