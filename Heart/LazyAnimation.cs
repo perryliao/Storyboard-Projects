5e3d5ba4f69320a8f8d51a97b5ffb71be5a929cc
Perry Liao <perryliaohc@gmail.com>
2019-07-01 22:59:07 -0700

    created base box animations

--- Heart/LazyAnimation.cs
+++ Heart/LazyAnimation.cs
@@ -12,8 +12,10 @@ using System.Linq;
 
 namespace StorybrewScripts
 {
+
     public class LazyAnimation : StoryboardObjectGenerator
     {
+        private double beatLength = 706;
         public override void Generate()
         {
 		    var layer = GetLayer("Lazy");
@@ -29,6 +31,26 @@ namespace StorybrewScripts
             circle.Move(OsbEasing.InQuad, 36644, 37350, circle.PositionAt(36644), 800, 240);
             circle.Fade(OsbEasing.InOutElastic, 36644, 37350, 1, 0);
             circle.Color(36821, 1, 0, 0);
+
+            double boxEndTime = 38233;
+            OsbSprite boxOut = setUpBox(37350, 40174);
+            boxOut.Scale(OsbEasing.Out, 37350, boxEndTime, boxOut.ScaleAt(37350).X, 1.5);
+            boxOut.Rotate(37350, boxEndTime, 0, 5*Math.PI/4);
+            OsbSprite boxMid = setUpBox(37350, 40174);
+            boxMid.Scale(OsbEasing.Out, 37527, boxEndTime, boxMid.ScaleAt(37527).X, 1);
+            boxMid.Rotate(37527, boxEndTime, 0, Math.PI);
+            OsbSprite boxIn = setUpBox(37350, 40174);
+            boxIn.Scale(OsbEasing.Out, 37703, boxEndTime, boxIn.ScaleAt(37703).X, 0.5);
+            boxIn.Rotate(37703, boxEndTime, 0, 3*Math.PI/4);
+
+        }
+
+        private OsbSprite setUpBox( double start, double end) {
+            OsbSprite box = GetLayer("boxy").CreateSprite("sb/boxy.png", OsbOrigin.Centre);
+            box.Fade(OsbEasing.In, start - beatLength/4, start, 0, 1);
+            box.Scale(start - beatLength/4, 0);
+            box.Fade(end, 0);
+            return box;
         }
     }
 }
