d6eb6938db462c3dd51060001a33bf18d644a319
Perry Liao <perryliaohc@gmail.com>
2019-07-03 22:26:49 -0700

    refactored rain code with a LoopGroup instead of sprite pool and use a different sprite

--- Heart/Rain.cs
+++ Heart/Rain.cs
@@ -29,32 +29,26 @@ namespace StorybrewScripts
             Func<double, double> degToRad = (deg) => Math.PI * deg / 180.0;
 		    var layer = GetLayer("Rain");
             
-            OsbOrigin Origin = OsbOrigin.Centre;
-            using (OsbSpritePool pool = new OsbSpritePool(layer, "sb/Pool 1/rain2.png", Origin, (rain2, startTime, endTime) => {
-                // No states common to every sprite here.
-            })) {
-                double timeStep = particleDuration / particleAmount;
-                double curStartTime, curEndTime;
-                for (curStartTime = startTime; curStartTime <= endTime - particleDuration; curStartTime += timeStep) {
-                    curEndTime = curStartTime + particleDuration;
-                    OsbSprite rain2 = pool.Get(curStartTime, curEndTime);
-                    double angle = Random(-5, 5);
-                    rain2.Rotate(curStartTime, degToRad(angle));
-                    rain2.ScaleVec(curStartTime, 1, Random(50, 200) / 100);
+            double i;
+            for (i = 0; i < particleAmount; i++) {
+                OsbSprite spec = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.Centre);
+                double angle = Random(-5, 5);
+                double length = Random(50, 200);
 
-                    double startX = Random(-107, 747), startY = -100; // Random(0, 480);
-                    if (angle < 0) {
-                        // points to the right, move right
-                        rain2.Move(OsbEasing.InOutExpo, curStartTime, curEndTime, startX, startY, startX + Random(0, 5), 600);
-                    } else {
-                        // points to the left, move left
-                        rain2.Move(OsbEasing.InOutExpo, curStartTime, curEndTime, startX, startY, startX - Random(0, 5), 600);
-                    }
-
-                    rain2.Fade(OsbEasing.InExpo, curStartTime, curStartTime + beatLength / 2, 0, 0.4);
-                    rain2.Fade(OsbEasing.OutExpo, curEndTime - beatLength / 2, curEndTime, 0.4, 0);
-                    
+                spec.StartLoopGroup(startTime + Random(0, beatLength*4), (int) ((endTime - startTime) / particleDuration - 2));
+                
+                spec.Rotate(0, degToRad(angle));
+                spec.ScaleVec(0, 1, length);
+                double startX = Random(-107, 747), startY = -100;
+                if (angle < 0) {
+                    // points to the right, move right
+                    spec.Move(OsbEasing.InOutExpo, 0, particleDuration, startX, startY, startX + Random(0, 5), 600);
+                } else {
+                    // points to the left, move left
+                    spec.Move(OsbEasing.InOutExpo, 0, particleDuration, startX, startY, startX - Random(0, 5), 600);
                 }
+                
+                spec.EndGroup();
             }
         }
     }
