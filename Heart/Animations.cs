45040df3245773e56fbedd9237ea4dedf7778b24
Perry Liao <perryliaohc@gmail.com>
2019-07-06 17:55:09 -0700

    completed triangle initial movements

--- Heart/Animations.cs
+++ Heart/Animations.cs
@@ -19,6 +19,7 @@ namespace StorybrewScripts
 		    var layer = GetLayer("Animations");
             Vector2 squareOrigin = new Vector2(30, 270);
             
+            // Square effect
             OsbSprite square;
             int i, rng = -1, prevRng = -1, numSquares = 21;
             double squareStart = 45821, squareEnd = 47939;
@@ -46,6 +47,32 @@ namespace StorybrewScripts
                 square.Fade(OsbEasing.InOutQuart, 47939, 48644, 1, 0);
             }
 
+            // Triangle/circle effect
+            float hypotenuse = 100;
+            float split = (float) Math.Cos(Math.PI/6) * hypotenuse;
+            float deltaY = (float)  Math.Sin(Math.PI/6) * hypotenuse;
+            Vector2 BottomMid = new Vector2(320, 240 + hypotenuse); // A
+            Vector2 TopLeft = new Vector2(320 - split, 240 - deltaY); // B
+            Vector2 TopRight = new Vector2(320 + split, 240 - deltaY); // C
+
+            OsbSprite AB = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.CentreRight, new Vector2((float)(TopLeft.X - Math.Tan(Math.PI/6)*TopLeft.Y), 0));
+            OsbSprite AC = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.CentreRight, new Vector2((float)( BottomMid.X - (480 - BottomMid.Y)/Math.Tan(Math.PI/3) ), 480));
+            OsbSprite BC = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.CentreLeft, new Vector2(747, TopLeft.Y));
+
+            OsbSprite[] Triangle = new OsbSprite[3] { AB, BC, AC };   
+
+            AB.Rotate(48468, Math.PI/3);
+            AC.Rotate(48468, -Math.PI/3);
+
+            foreach( OsbSprite line in Triangle ) {
+                line.Fade(OsbEasing.InExpo, 48468, 48644, 0, 1);
+                line.ScaleVec(48468, 50762, 250, 2, 174, 2);
+                line.Fade(51468, 0);
+            }
+
+            AB.Move(OsbEasing.InOutExpo, 48468, 49350, AB.PositionAt(48468), BottomMid.X, BottomMid.Y);
+            AC.Move(OsbEasing.InOutExpo, 48468, 49350, AC.PositionAt(48468), TopRight.X, TopRight.Y);
+            BC.Move(OsbEasing.InOutExpo, 48468, 49350, BC.PositionAt(48468), TopLeft.X, TopLeft.Y);
         }
     }
 }
