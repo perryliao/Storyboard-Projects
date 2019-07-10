859c3af0aac710dbdbb97c725a0122964dece7c7
Perry Liao <perryliaohc@gmail.com>
2019-07-09 17:58:05 -0700

    update colours in rain animations

--- Heart/scriptslibrary/Constants.cs
+++ Heart/scriptslibrary/Constants.cs
@@ -16,9 +16,16 @@ namespace StorybrewScripts
         public static double beatLength = 706;
         
         public static Color4 red = new Color4((float)225/255, (float)9/255, (float)11/255, 1f);
+        public static Color4 blue = new Color4(0, 0.8f, 0.9f, 1f);
         public static Color4 black = new Color4((float)26/255, (float)26/255, (float)26/255, 1f);
         public static Color4 white = new Color4((float) (242f/255), (float) (242f/255), (float) (242f/255), 1f);
         public static Color4 grey = new Color4(0.5f, 0.5f, 0.5f, 1f);
 
+        public static Color4[] colours = new Color4[4] {
+            white,
+            black,
+            grey,
+            red
+        };
     }
 }
