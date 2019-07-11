aa2c89e2e4e23e24d491f3ee9d6cc86797416401
Perry Liao <perryliaohc@gmail.com>
2019-07-11 01:43:04 -0700

    finished KokOwOro ^-^

--- Heart/scriptslibrary/Constants.cs
+++ Heart/scriptslibrary/Constants.cs
@@ -18,13 +18,14 @@ namespace StorybrewScripts
         public static Color4 white = new Color4((float) (242f/255), (float) (242f/255), (float) (242f/255), 1f);
         public static Color4 grey = new Color4(0.5f, 0.5f, 0.5f, 1f);
 
-        public static Color4[] randomColours = new Color4[13] {
+        public static Color4[] randomColours = new Color4[14] {
             new Color4((float) (246f/255), (float) (4f/255), (float) (88f/255), 1f),    // #f60458
             new Color4(1, (float) (195f/255), (float) (123f/255), 1f),                  // #ffc37b
             new Color4((float) (64f/255), (float) (201f/255), (float) (112f/255), 1f),  // #40c970
             new Color4((float) (148f/255), (float) (135f/255), (float) (245f/255), 1f), // #9487f5
             new Color4((float) (197f/255), (float) (135f/255), (float) (245f/255), 1f), // #c587f5
             new Color4((float) (245f/255), (float) (155f/255), (float) (234f/255), 1f), // #f587ea
+            new Color4((float) (11f/255), (float) (249f/255), (float) (234f/255), 1f),  // #0bf9ea
             new Color4(1, (float) (214f/255), (float) (123f/255), 1f),                  // #ffd67b
             new Color4((float) (146f/255), 1, (float) (123f/255), 1f),                  // #92ff7b
             new Color4((float) (123f/255), 1, (float) (212f/255), 1f),                  // #7bffd4
