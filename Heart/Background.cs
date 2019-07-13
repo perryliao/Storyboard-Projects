9d890156d8d39a1cf8bd4afcaed422ec176083ed
Perry Liao <perryliaohc@gmail.com>
2019-07-13 00:34:01 -0700

    add poison bg progress

--- Heart/Background.cs
+++ Heart/Background.cs
@@ -1,6 +1,9 @@
-﻿using StorybrewCommon.Scripting;
+﻿using OpenTK.Graphics;
+using StorybrewCommon.Scripting;
 using StorybrewCommon.Storyboarding;
+using System;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace StorybrewScripts
 {
@@ -18,9 +21,21 @@ namespace StorybrewScripts
         [Configurable]
         public double Opacity = 0.2;
 
-        
-        private int width = 854;
-        private int height = 480;
+        public double[] poisonArr = new double[13] {
+            69468,
+            69644,
+            69821, 
+            70350,
+            71233,
+            72292, 
+            72468,
+            72644,
+            73174, 
+            73703, 
+            74056,
+            74586,
+            75115,
+        };
 
         public override void Generate()
         {
@@ -35,20 +50,34 @@ namespace StorybrewScripts
             bg.Fade(StartTime, 0);
 
             OsbSprite tileBG = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.Centre);
-            tileBG.ColorHsb(34527, 0, 0, 0.1);
-            tileBG.ScaleVec(34527, width, height);
+            tileBG.Color(34527, Constants.black);
+            tileBG.ScaleVec(34527, Constants.width, Constants.height);
             tileBG.Fade(OsbEasing.OutExpo, 33821, 34527, 0, 0.6);
             tileBG.Fade(42997, 0);
 
             OsbSprite letterboxBG = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.Centre);
             letterboxBG.ColorHsb(23233, 51, 0.09, 0.9);
             letterboxBG.Fade(23233, 1);
-            letterboxBG.ScaleVec(23233, width, height * 0.7);
+            letterboxBG.ScaleVec(23233, Constants.width, Constants.height * 0.7);
             letterboxBG.Fade(OsbEasing.InCirc, 33821, 34527, 1, 0);
-
             
+            poisonBG(tileBG);
+
             letterboxBG.Fade(209586, 1);
             letterboxBG.Fade(232174, 0);
+        }
+
+        public void poisonBG(OsbSprite bg) {
+            double start, end;
+            Color4 colour;
+            bg.Fade(69468, 0.4);
+            foreach(double time in poisonArr) {
+                start = time - Constants.beatLength/4;
+                end = time;
+                colour = Constants.randomColours[Random(Constants.randomColours.Length)];
+                bg.Color(OsbEasing.InExpo, start, end, bg.ColorAt(start), colour);
+                // bg.Fade(start, end, bg.OpacityAt(start), 0.6);
+            }
         }
     }
 }
--- Heart/scriptslibrary/Constants.cs
+++ Heart/scriptslibrary/Constants.cs
@@ -17,6 +17,7 @@ namespace StorybrewScripts
         public static Color4 black = new Color4((float)26/255, (float)26/255, (float)26/255, 1f);
         public static Color4 white = new Color4((float) (242f/255), (float) (242f/255), (float) (242f/255), 1f);
         public static Color4 grey = new Color4(0.5f, 0.5f, 0.5f, 1f);
+        public static Color4 transparent = new Color4(0, 0, 0, 0);
 
         public static Color4[] randomColours = new Color4[14] {
             new Color4((float) (246f/255), (float) (4f/255), (float) (88f/255), 1f),    // #f60458
