1d98b50abd7b0804c573d5dedb986bd993c963f8
Perry Liao <perryliaohc@gmail.com>
2019-07-13 18:50:52 -0700

    removed use of sprite array

--- Heart/BeatDots.cs
+++ Heart/BeatDots.cs
@@ -20,28 +20,30 @@ namespace StorybrewScripts
         public double endTime = 113586;
         [Configurable]
         public double scale = 0.2;
+        [Configurable]
+        public int numDots = 4;
 
         public override void Generate()
         {
 		    StoryboardLayer layer = GetLayer("BeatDots");
-            OsbSprite[] dots = new OsbSprite[4];
-            OsbSprite dotAni;
-            double d = Constants.width / (dots.Length + 1);
+            OsbSprite dot, dotAni;
+            double d = Constants.width / (numDots + 1);
             double frameDelay = 8;
             int numIterations = (int)((endTime - startTime) / (Constants.beatLength*4) + 1);
 
-            for (int i = 0; i < dots.Length; i++) {
+            for (int i = 0; i < numDots; i++) {
                 double relativeStart = startTime + Constants.beatLength/2 + i*Constants.beatLength*3/4;
                 double loopEnd = startTime + Constants.beatLength*7/2 - relativeStart;
                 Vector2 pos = new Vector2((float)(d*(i+1) + Constants.xFloor), 240);
-                dots[i] = layer.CreateSprite("sb/Pool 3/Animation_1/Ellipse1.png", OsbOrigin.Centre, pos);
-                dots[i].Scale(relativeStart, scale);
 
-                dots[i].StartLoopGroup(relativeStart, numIterations);
-                dots[i].Fade(0, 1);
-                dots[i].Fade(loopEnd, 0);
-                dots[i].Color(Constants.beatLength * 4, Constants.white);
-                dots[i].EndGroup();
+                dot = layer.CreateSprite("sb/Pool 3/Animation_1/Ellipse1.png", OsbOrigin.Centre, pos);
+                dot.Scale(relativeStart, scale);
+
+                dot.StartLoopGroup(relativeStart, numIterations);
+                dot.Fade(0, 1);
+                dot.Fade(loopEnd, 0);
+                dot.Color(Constants.beatLength * 4, Constants.white);
+                dot.EndGroup();
 
                 dotAni = layer.CreateAnimation("sb/Pool 3/Animation_1/Ellipse.png", 32, frameDelay, OsbLoopType.LoopForever, OsbOrigin.Centre, pos);
                 dotAni.StartLoopGroup(relativeStart + loopEnd, numIterations);
