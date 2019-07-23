7cbf14614dfa72a79d0bfebc912b5037ea67fff0
Perry Liao <perryliaohc@gmail.com>
2019-07-22 22:41:54 -0700

    used bghs texture as 3rd bar

--- duet/ScrollingLyricBG.cs
+++ duet/ScrollingLyricBG.cs
@@ -25,9 +25,19 @@ namespace StorybrewScripts
             layer = GetLayer("ScrollingLyricBG");
             OsbSprite bar1 = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.BottomCentre);
             OsbSprite bar2 = layer.CreateSprite("sb/1x1.jpg", OsbOrigin.BottomCentre);
+            OsbSprite bar3 = layer.CreateSprite("sb/othersbg.jpg", OsbOrigin.Centre);
+            var bar3BitMap = GetMapsetBitmap("sb/othersbg.jpg");
             
             handleLyricBar(bar1, startTime, -Math.PI/Random(16, 32), Colours.blue);
             handleLyricBar(bar2, startTime + Constants.beatLength*4, Math.PI/Random(16, 32), Colours.pink);
+
+            double finalIn = startTime + Constants.beatLength * 8;
+            double finalOut = finalIn + Constants.beatLength * 12;
+            
+            bar3.Fade(finalIn, finalIn + Constants.beatLength/2, 0, 1);
+            bar3.Fade(finalOut - Constants.beatLength*4, finalOut, 1, 0);
+            bar3.Scale(finalIn, 480.0f / bar3BitMap.Height);
+            // bar3.Move(finalIn, finalIn + Constants.beatLength/2, 320, Constants.height + )
         }
 
         private void handleLyricBar(OsbSprite bar, double start, double rotation, Color4 colour) {
